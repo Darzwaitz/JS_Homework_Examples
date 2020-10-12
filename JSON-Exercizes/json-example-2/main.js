52eab1a8e9a9bc7a1eadf44e453fc57f3b7f9026
Darren Kelly <darrenkelly@fb.com>
2020-10-12 13:46:58 +0100

    added JSON examples 1 and 2

---
+++ JSON-Exercizes/json-example-2/main.js
@@ -0,0 +1,28 @@
+var MAINAPP = (function(app) {
+
+    let jsonObj = {};
+
+    let loadJSON = function(path) {
+        let xobj = new XMLHttpRequest();
+        xobj.overrideMimeType('application/json');
+        xobj.open('GET', path);
+        xobj.onreadystatechange = function() {
+            if (xobj.readyState === 4) {
+                app.jsonObj = JSON.parse(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    };
+
+    //Public methods
+    app.jsonObj = jsonObj;
+    app.loadJSON = loadJSON;
+
+    return app;
+})(MAINAPP || {});
+
+(function init() {
+    MAINAPP.loadJSON('data.json');
+    console.log(MAINAPP.jsonObj.title);
+
+})();
