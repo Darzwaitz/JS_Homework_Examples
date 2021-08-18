9050c9bfa0aef80c26b9649c81aa0fa33343954b
darzwaitz <darzwaitz@yahoo.com>
2021-08-18 23:59:26 +0100

    Exercise - 13 (spread and forEach) UPDATED

--- JS-Arrays(in depth)/Exercise - 13 (spread and forEach)/js/app.js
+++ JS-Arrays(in depth)/Exercise - 13 (spread and forEach)/js/app.js
@@ -8,6 +8,27 @@ let studentsA = ["James", "Annika", "Cameron", "AntoniO", "Kaylie", "Gabriel", "
 
 let studentsB = ["LJ", "Brianna", "David", "Antonio", "Kelsey", "Joshua", "Emily", "Sarah"];
 
+// create array from set(delete duplicates) - convert all elements to lowercase with map
 let allStudents = Array.from(new Set([...studentsA.map(val => val.toLowerCase()), ...studentsB.map(val => val.toLowerCase())]));
 
-console.log(allStudents);
+console.log(allStudents); // Array(13) [ "james", "annika", "cameron", "antonio", "kaylie", "gabriel", "emily", "sarah", "lj", "brianna", … ]
+
+// convert 1st letter elements to uppercase, and add the rest of that element with slice method
+let allStudentsFinal = allStudents.forEach(name => console.log(name[0].toUpperCase() + name.slice(1, name.length)));
+
+// console.log(allStudents);
+console.log(allStudentsFinal);
+// James
+// Annika 
+// Cameron 
+// Antonio 
+// Kaylie 
+// Gabriel 
+// Emily 
+// Sarah 
+// Lj 
+// Brianna 
+// David 
+// Kelsey 
+// Joshua 
+// undefined
