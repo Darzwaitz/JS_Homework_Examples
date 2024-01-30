f1765e97c3ab3ac3679f75da9b528d690ab42f41
darzwaitz <darzwaitz@yahoo.com>
2024-01-30 14:58:38 +0000

    reverse string example

--- JS-Data Structures and Algorithms/01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ JS-Data Structures and Algorithms/01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,18 @@
-function reverseString() {}
+// // long way
+// function reverseString(str) {
+//   let strL = str.length - 1;
+//   let nxtEl = [];
+
+//   for (let i = 0; i < strL + 1; i++) {
+//     nxtEl.push(str[strL - i]);
+//   }
+
+//   return nxtEl.join("");
+// }
+
+// short way
+const reverseString = (str) => str.split("").reverse().join("");
+
+console.log(reverseString("Hello"));
 
 module.exports = reverseString;
