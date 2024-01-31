9c88c5d8e0411d1f0bdf54fa6bfc285fdc0cb5d3
darzwaitz <darzwaitz@yahoo.com>
2024-01-31 21:20:23 +0000

    palindrome example 1

--- JS-Data Structures and Algorithms/01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ JS-Data Structures and Algorithms/01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -11,8 +11,18 @@
 // }
 
 // short way
-const reverseString = (str) => str.split("").reverse().join("");
+// const reverseString = (str) => str.split("").reverse().join("");
 
-console.log(reverseString("Hello"));
+// 3rd way
+function reverseString(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+// console.log(reverseString("Hello"));
 
 module.exports = reverseString;
