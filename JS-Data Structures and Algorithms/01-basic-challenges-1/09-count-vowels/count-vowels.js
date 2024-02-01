ac2f3930ac741f55cfe7e2db8c8d62e49bcadcc9
darzwaitz <darzwaitz@yahoo.com>
2024-02-01 16:34:04 +0000

    examples counting vowels

--- JS-Data Structures and Algorithms/01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ JS-Data Structures and Algorithms/01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,33 @@
-function countVowels() {}
+// Solution 1
+// function countVowels(str) {
+
+//   const output = [];
+//   const input = str.toLowerCase().split("");
+
+//   for (i = 0; i < input.length; i++) {
+//     let cur = input[i];
+//     if (
+//       cur === "a" ||
+//       cur === "e" ||
+//       cur === "i" ||
+//       cur === "o" ||
+//       cur === "u"
+//     ) {
+//       output.push(input[i]);
+//     }
+//   }
+//   return output.length;
+// }
+
+// Solution 2
+function countVowels(str) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  const input = str
+    .toLowerCase()
+    .split("")
+    .filter((el) => vowels.includes(el)).length;
+
+  return input;
+}
 
 module.exports = countVowels;
