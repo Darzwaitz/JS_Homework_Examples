088fd4bedbd45e727f6cc0d4bb84beab2191b332
darzwaitz <darzwaitz@yahoo.com>
2024-02-01 17:32:32 +0000

    examples remove duplicates

--- JS-Data Structures and Algorithms/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ JS-Data Structures and Algorithms/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,23 @@
-function removeDuplicates() {}
+// Solution 1
+// function removeDuplicates(arr) {
+//   return Array.from(new Set(arr));
+// }
+
+// Solution 2
+function removeDuplicates(arr) {
+  const output = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let cur = arr[i];
+    if (!output.includes(cur)) {
+      output.push(cur);
+    }
+  }
+  return output;
+}
+
+console.log(
+  removeDuplicates([1, 2, 3, 4, 5, 5, 5, 6, 7, 8, "hello", "hello", true, true])
+);
 
 module.exports = removeDuplicates;
