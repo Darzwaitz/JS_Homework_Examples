705ebca7eda57f74d3dffed9dc984b86c0f392e2
darzwaitz <darzwaitz@yahoo.com>
2024-02-02 17:10:12 +0000

    array intersection 2 solutions

--- JS-Data Structures and Algorithms/02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ JS-Data Structures and Algorithms/02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,28 @@
-function arrayIntersection() {}
+// solution 1
+function arrayIntersection(arr1, arr2) {
+  let outputArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    let curIndex = arr1[i];
+    if (arr2.includes(curIndex)) outputArr.push(curIndex);
+  }
+  return outputArr;
+}
+
+// solution 2
+// function arrayIntersection(arr1, arr2) {
+//   const set1 = new Set(arr1);
+//   const outputArr = [];
+
+//   for (const num of arr2) {
+//     if (set1.has(num)) {
+//       outputArr.push(num);
+//     }
+//   }
+
+//   return outputArr;
+// }
+
+// console.log(arrayIntersection([1, 2, 3, 4, 5], [1, 3, 5, 7, 9]));
 
 module.exports = arrayIntersection;
