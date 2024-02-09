726cbf1cf134d879cd15c8c4aabf7a38b4adc6ad
darzwaitz <darzwaitz@yahoo.com>
2024-02-09 19:40:23 +0000

    2 solutions - missing letter

--- JS-Data Structures and Algorithms/02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ JS-Data Structures and Algorithms/02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,47 @@
-function findMissingLetter() {}
+// solution 1
+// function findMissingLetter(arr) {
+//   const passedIn = arr.toString().split(",");
+
+// // const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ";
+//   const alphabet = [..."abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ"];
+
+//   let firstLetAlph = alphabet[alphabet.indexOf(passedIn[0])];
+//   let secondLetAlph = alphabet[alphabet.indexOf(passedIn[passedIn.length - 1])];
+
+//   let range = alphabet.slice(
+//     alphabet.indexOf(firstLetAlph),
+//     alphabet.indexOf(secondLetAlph) + 1
+//   );
+
+//   const missingLett = [];
+
+//   range.forEach((element) => {
+//     if (!passedIn.includes(element)) {
+//       missingLett.push(element);
+//     }
+//   });
+
+//   return missingLett.toString();
+// }
+
+// solution 2
+function findMissingLetter(arr) {
+  let start = arr[0].charCodeAt();
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i].charCodeAt();
+
+    if (current - start > 1) {
+      return String.fromCharCode(start + 1);
+    }
+
+    start = current;
+  }
+
+  return "";
+  // console.log(start);
+}
+
+// console.log(findMissingLetter(["t", "u", "v", "w", "x", "z"]));
 
 module.exports = findMissingLetter;
