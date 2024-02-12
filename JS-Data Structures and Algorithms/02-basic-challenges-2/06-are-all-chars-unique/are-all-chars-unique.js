7122b609e75d9cb6c70832807f6100e0377811d7
darzwaitz <darzwaitz@yahoo.com>
2024-02-12 20:05:14 +0000

    solutions all unique chars and first non repeating char

--- JS-Data Structures and Algorithms/02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ JS-Data Structures and Algorithms/02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,8 @@
-function areAllCharactersUnique() {}
+// Solution 1
+function areAllCharactersUnique(str) {
+  const strSet = new Set(str).size;
+  if (strSet === str.length) return true;
+  return false;
+}
 
 module.exports = areAllCharactersUnique;
