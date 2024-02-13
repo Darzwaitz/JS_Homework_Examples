c6077ebc45c9e877532d5a13a1bb7644d0b7a773
darzwaitz <darzwaitz@yahoo.com>
2024-02-13 22:51:01 +0000

    format number no regex and test

--- JS-Data Structures and Algorithms/02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ JS-Data Structures and Algorithms/02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,11 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbers) {
+  numbers = numbers.join("");
+  const part1 = numbers.slice(0, 3);
+  const part2 = numbers.slice(3, 6);
+  const part3 = numbers.slice(6, numbers.length);
+  return `(${part1}) ${part2}-${part3}`;
+}
+
+// console.log(formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 
 module.exports = formatPhoneNumber;
