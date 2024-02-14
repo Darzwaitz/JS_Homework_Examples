dc23714acf39be2d21077819ca78737d88574e78
darzwaitz <darzwaitz@yahoo.com>
2024-02-14 22:33:48 +0000

    one solution - validate email with regex

--- JS-Data Structures and Algorithms/02-basic-challenges-2/10-validate-email/validate-email.js
+++ JS-Data Structures and Algorithms/02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,12 @@
-function validateEmail() {}
+function validateEmail(email) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(validateEmail("john@gmail.com"));
+console.log(validateEmail("john@gmail"));
 
 module.exports = validateEmail;
