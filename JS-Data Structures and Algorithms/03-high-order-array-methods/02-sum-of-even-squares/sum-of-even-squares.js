d13175976345805444ddbb5e830f6e9ce21a7b61
darzwaitz <darzwaitz@yahoo.com>
2024-02-28 18:23:25 +0000

    sum of even squares

--- JS-Data Structures and Algorithms/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ JS-Data Structures and Algorithms/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,19 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  return (
+    numbers
+      .filter((element) => {
+        if (element % 2 === 0) {
+          return element;
+        }
+      })
+      .map((even) => even * even)
+      //   .map((even) => Math.pow(even, 2))
+      //   .map((even) => even ** 2)
+      .reduce((result, elem) => result + elem, 0)
+  );
+}
+
+// sumOfEvenSquares(numbers);
+console.log(sumOfEvenSquares([1, 2, 3, 4, 5]));
 
 module.exports = sumOfEvenSquares;
